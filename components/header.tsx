11645a50fca628b652abb82df877dfbfbe594a69
MhemedAbderrahmen <mhemedabderrahmen@gmail.com>
2026-01-26 06:02:23 +0100

    feat: x account

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,8 @@
+import { HugeiconsIcon } from "@hugeicons/react";
 import { Button } from "./ui/button";
 
 import Link from "next/link";
+import { NewTwitterIcon } from "@hugeicons/core-free-icons";
 export function Header() {
   return (
     <header className="border-b border-border">
@@ -9,14 +11,17 @@ export function Header() {
           <span className="h-3 w-3 rounded-full bg-primary" />
           PayOnce
         </Link>
-        <nav className="flex gap-4 text-sm">
+        <nav className="flex gap-4 text-sm items-center">
+          <Link target="_blank" href="https://x.com/codepinedev" title="Dev Account">
+            <HugeiconsIcon icon={NewTwitterIcon} size="16"/>
+          </Link>
           <Link
             href="/tools"
             className="text-muted-foreground hover:text-foreground"
           >
-  <Button variant={"outline"}>
-            Browse
-  </Button>
+            <Button variant={"outline"}>
+              Browse
+            </Button>
 
           </Link>
           <Link
